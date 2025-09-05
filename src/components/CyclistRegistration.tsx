7cff3bc88a7f414423deb48873a04fcde11dc9cf
iaexpertia <iaexpertia@gmail.com>
2025-09-05 13:47:08 +0000

    Fix useEffect import error

--- src/components/CyclistRegistration.tsx
+++ src/components/CyclistRegistration.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Cyclist, Bike } from '../types';
 import { Translation } from '../i18n/translations';
 import { addCyclist } from '../utils/cyclistStorage';
